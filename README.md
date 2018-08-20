d5191669033b47f270a034d5402a0279382e1d15
nonko8 <git@nonko8.com>
2018-08-20 16:24:13 +0900

    CSharp QnAMaker Projectの追加

--- README.md
+++ README.md
@@ -23,5 +23,64 @@ Azure の Coginitive Services のひとつ。
 |Azure Search (for the data)|[More information](https://azure.microsoft.com/ja-jp/services/search/)|[Pricing](https://azure.microsoft.com/ja-jp/pricing/details/search/)|
 
 ### 作成
-ダッシュボードから「リソースの作成」で「QnA Maker」で検索する。
+ダッシュボードから「リソースの作成」で「QnA Maker」で検索して作成する。
 
+## APIの作成（C#編）
+### キーの取得
+QnA Makerをリソースとして選択したAPIアカウントが必要になるので、Azureダッシュボードダッシュボードの[リソース管理]で[キー]を選択し、どこかにコピー＆ペーストしておく。  
+キーは２つ表示されるが、どちらか一方だけ(KEY1だけ)でOK。
+
+![2018-08-20_152909.png](images/2018-08-20_152909.png)
+
+### ナレッジベース生成
+QnAMakerのナレッジベースを生成するアプリを作成する。
+
+1. .Net Frameowrk のコンソールアプリケーションプロジェクトを作成
+2. プロジェクトを作成したら、NuGetから「Newtonsoft.JSON」をインストール
+3. `Programs.cs` を以下のコードに置き換える
+<script src="https://gist.github.com/nonko8/aeadcafc852ca712325d27511fbb9b35.js"></script>
+4. `key` をAzureダッシュボードからコピーした値に置き換える
+
+### ナレッジベース生成時のレスポンス
+レスポンスはJSONで返される。最後の呼び出しが「`Succeeded`」を返した場合、ナレッジベースが正常に作成されたことを表す。トラブルシューティングを行うには、[QnA Maker APIの操作の詳細を取得する](https://westus.dev.cognitive.microsoft.com/docs/services/5a93fcf85b4ccd136866eb37/operations/operations_getoperationdetails)を参照する。
+
+```json
+Calling https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/knowledgebases/create.
+{
+  "operationState": "NotStarted",
+  "createdTimestamp": "2018-06-25T10:30:15Z",
+  "lastActionTimestamp": "2018-06-25T10:30:15Z",
+  "userId": "0d85ec291c204197a70cfec51725cd22",
+  "operationId": "d9d40918-01bd-49f4-88b4-129fbc434c94"
+}
+Calling https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/operations/d9d40918-01bd-49f4-88b4-129fbc434c94.
+{
+  "operationState": "Running",
+  "createdTimestamp": "2018-06-25T10:30:15Z",
+  "lastActionTimestamp": "2018-06-25T10:30:15Z",
+  "userId": "0d85ec291c184197a70cfeb51025cd22",
+  "operationId": "d9d40918-01bd-49f4-88b4-129fbc434c94"
+}
+Waiting 30 seconds...
+Calling https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/operations/d9d40918-01bd-49f4-88b4-129fbc434c94.
+{
+  "operationState": "Running",
+  "createdTimestamp": "2018-06-25T10:30:15Z",
+  "lastActionTimestamp": "2018-06-25T10:30:15Z",
+  "userId": "0d85ec221c284197a70gfeb51725cd22",
+  "operationId": "d9d40918-01bd-49f4-88b4-129fbc434c94"
+}
+Waiting 30 seconds...
+Calling https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/operations/d9d40918-01bd-49f4-88b4-129fbc434c94.
+{
+  "operationState": "Succeeded",
+  "createdTimestamp": "2018-06-25T10:30:15Z",
+  "lastActionTimestamp": "2018-06-25T10:30:51Z",
+  "resourceLocation": "/knowledgebases/1d9eb2a1-de2a-4709-91b2-f6ea8afb6fb9",
+  "userId": "0d85ec294c284197a70cfeb51775cd22",
+  "operationId": "d9d40918-01bd-49f4-88b4-129fbc434c94"
+}
+Press any key to continue.
+```
+
+ナレッジベースが作成されると、[QnA Makerポータル](https://www.qnamaker.ai/Home/MyServices)の[ナレッジベース]ページで、。表示するナレッジベース名（QnA Maker FAQなど）を選択すると、ナレッジベースを表示できる。
---
+++ src/QnAMaker/QnAMaker/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Configuration;
+
+// NOTE: Newtonsoft.Json NuGet パッケージをインストールしてください.
+using Newtonsoft.Json;
+
+namespace QnAMaker
+{
+    class Program
+    {
+        // QnA MakerのHTTPリクエストURIを作成するため設定.
+        static string host = "https://westus.api.cognitive.microsoft.com";
+        static string service = "/qnamaker/v4.0";
+        static string method = "/knowledgebases/create";
+
+        // NOTE: Azureダッシュボードからコピーしたキーに置き換えてください.
+        static string key = ConfigurationManager.AppSettings["QnAMakerSubscriptionKey"];
+
+        /// <summary>
+        /// ナレッジベースの作成に使用されるデータソースを定義.
+        /// データソースには、QnAペア、メタデータ、QnA Maker FAQのURL、およびAzure BotサービスFAQのURLが含まれる.
+        /// </summary>
+        static string kb = @"
+{
+  'name': 'QnA Maker FAQ',
+  'qnaList': [
+    {
+      'id': 0,
+      'answer': 'You can use our REST APIs to manage your knowledge base. See here for details: https://westus.dev.cognitive.microsoft.com/docs/services/58994a073d9e04097c7ba6fe/operations/58994a073d9e041ad42d9baa',
+      'source': 'Custom Editorial',
+      'questions': [
+        'How do I programmatically update my knowledge base?'
+      ],
+      'metadata': [
+        {
+          'name': 'category',
+          'value': 'api'
+        }
+      ]
+    }
+  ],
+  'urls': [
+    'https://docs.microsoft.com/ja-jp/azure/cognitive-services/qnamaker/faqs',
+    'https://docs.microsoft.com/ja-jp/bot-framework/resources-bot-framework-faq'
+  ],
+  'files': []
+}
+";
+
+        /// <summary>
+        /// HTTPリクエストによって返されるHTTPレスポンス.
+        /// </summary>
+        public struct Response
+        {
+            public HttpResponseHeaders headers;
+            public string response;
+
+            public Response(HttpResponseHeaders headers, string response)
+            {
+                this.headers = headers;
+                this.response = response;
+            }
+        }
+
+        /// <summary>
+        /// 表示用JSONの書式設定とインデント.
+        /// </summary>
+        /// <param name="s">JSONの書式設定とインデント.</param>
+        /// <returns>書式設定されインデントされたJSONを含む文字列.</returns>
+        static string PrettyPrint(string s)
+        {
+            return JsonConvert.SerializeObject(JsonConvert.DeserializeObject(s), Formatting.Indented);
+        }
+
+        /// <summary>
+        /// HTTPリクエストの非同期POST通信.
+        /// </summary>
+        /// <param name="uri">HTTPリクエストURI.</param>
+        /// <param name="body">HTTPリクエストBODY.</param>
+        /// <returns>HTTP <see cref="System.Threading.Tasks.Task{TResult}(QnAMaker.Program.Response)"/></returns>
+        async static Task<Response> Post(string uri, string body)
+        {
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return new Response(response.Headers, responseBody);
+            }
+        }
+
+        /// <summary>
+        /// HTTPリクエストの非同期GET通信.
+        /// </summary>
+        /// <param name="uri">HTTPリクエストURI.</param>
+        /// <returns>HTTP <see cref="System.Threading.Tasks.Task{TResult}(QnAMaker.Program.Response)"/></returns>
+        async static Task<Response> Get(string uri)
+        {
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Get;
+                request.RequestUri = new Uri(uri);
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return new Response(response.Headers, responseBody);
+            }
+        }
+
+        /// <summary>
+        /// ナレッジベースの作成.
+        /// </summary>
+        /// <param name="kb">ナレッジベースのデータソース.</param>
+        /// <returns>HTTP <see cref="System.Threading.Tasks.Task{TResult}(QnAMaker.Program.Response)"/></returns>
+        /// <remarks>
+        /// QnA Makerでナレッジベースを作成するためのURIを生成し, 
+        /// HTTPリクエストの非同期 <see cref="QnAMaker.Program.Post(string, string)"/> メソッドを呼び出す.
+        /// </remarks>
+        async static Task<Response> PostCreateKB(string kb)
+        {
+            string uri = host + service + method;
+
+            Console.WriteLine("Calling " + uri + ".");
+
+            return await Post(uri, kb);
+        }
+
+        /// <summary>
+        /// 指定されたQnA Makerオペレーションのステータスを取得.
+        /// </summary>
+        /// <param name="operation">The QnA Maker operation to check.</param>
+        /// <returns>HTTP <see cref="System.Threading.Tasks.Task{TResult}(QnAMaker.Program.Response)"/></returns>
+        /// <remarks>
+        /// QnA Makerでナレッジベースを作成するためのURIを生成し, 
+        /// HTTPリクエストの非同期  <see cref="QnAMaker.Program.Get(string)"/> メソッドを呼び出す.
+        /// </remarks>
+        async static Task<Response> GetStatus(string operation)
+        {
+            string uri = host + service + operation;
+
+            Console.WriteLine("Calling " + uri + ".");
+
+            return await Get(uri);
+        }
+
+        /// <summary>
+        /// ナレッジベースを作成し、ナレッジベースが作成されるまで定期的にステータスをチェックする
+        /// </summary>
+        async static void CreateKB()
+        {
+            try
+            {
+                var response = await PostCreateKB(kb);
+
+                var operation = response.headers.GetValues("Location").First();
+
+                Console.WriteLine(PrettyPrint(response.response));
+
+                var done = false;
+                while (true != done)
+                {
+                    response = await GetStatus(operation);
+
+                    Console.WriteLine(PrettyPrint(response.response));
+
+                    var fields = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.response);
+
+                    String state = fields["operationState"];
+                    if (state.CompareTo("Running") == 0 || state.CompareTo("NotStarted") == 0)
+                    {
+                        // QnA Maker ナレッジベースの作成中.
+                        // スレッドは、Retry-Afterヘッダー値と同じ秒数だけ一時停止され、ループが続行される.
+                        var wait = response.headers.GetValues("Retry-After").First();
+                        Console.WriteLine("Waiting " + wait + " seconds...");
+                        Thread.Sleep(Int32.Parse(wait) * 1000);
+                    }
+                    else
+                    {
+                        //  QnA Maker ナレッジベースの作成完了. 
+                        done = true;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("An error occurred while creating the knowledge base.");
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to continue.");
+            }
+
+        }
+
+        /// <summary>
+        /// エントリーポイント
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // CreateKB（）メソッドを呼び出してナレッジベースを作成し、
+            // ナレッジベースが作成されるまでQnA Maker操作のステータスを定期的にチェック.
+            CreateKB();
+
+            Console.ReadLine();
+        }
+    }
+}
